f28a622640b8d69d6c67d3492ea5daddb80bcd5d
Carl Waldbieser <cwaldbieser@gmail.com>
2024-07-22 14:26:05 -0400

    [chore] layout
    [chore] layout

--- gmail_tui.py
+++ gmail_tui.py
@@ -1,17 +1,45 @@
 #! /usr/bin/env python
 
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Button, Footer, Header, Static
+
+
+class Messages(Static):
+    pass
+
+
+class ButtonBar(Static):
+    def compose(self):
+        yield Button("<", disabled=True, id="btn-backwards", classes="button")
+        yield Button(">", disabled=True, id="btn-forwards", classes="button")
+
+
+class MessageList(Static):
+    def compose(self):
+        yield Messages("List of messages")
+        yield ButtonBar()
+
+
+class MessagePane(Static):
+    pass
+
+
+class MainPanel(Static):
+    def compose(self):
+        yield MessageList()
+        yield MessagePane("Message text is here.")
 
 
 class GMailApp(App):
     """A Textual app to manage stopwatches."""
 
+    CSS_PATH = "gmail_app.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
+        yield MainPanel()
         yield Footer()
 
     def action_toggle_dark(self) -> None:
