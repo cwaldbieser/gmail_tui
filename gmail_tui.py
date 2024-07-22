e473c9c889d6e82d08b88a70fdef9fec1b699900
Carl Waldbieser <cwaldbieser@gmail.com>
2024-07-22 17:08:58 -0400

    [chore] load message summaries
    [chore] load message summaries

--- gmail_tui.py
+++ gmail_tui.py
@@ -1,11 +1,38 @@
 #! /usr/bin/env python
 
+import pathlib
+
+import tomllib
+from dateutil.parser import parse as parse_date
+from textual import work
 from textual.app import App, ComposeResult
+from textual.containers import ScrollableContainer
+from textual.message import Message
 from textual.widgets import Button, Footer, Header, Static
 
+from gmailtuilib.gmailapi import get_gmail_credentials, list_messages
 
-class Messages(Static):
-    pass
+
+class MessageItem(Static):
+    def __init__(self, date_str, sender, subject):
+        self.date_str = date_str
+        self.sender = sender
+        self.subject = subject
+        super().__init__()
+
+    def compose(self):
+        yield Static(self.date_str)
+        yield Static(self.sender)
+        yield Static(self.subject)
+
+
+class Messages(ScrollableContainer):
+
+    class Mounted(Message):
+        pass
+
+    def on_mount(self):
+        self.post_message(self.Mounted())
 
 
 class ButtonBar(Static):
@@ -16,18 +43,18 @@ class ButtonBar(Static):
 
 class MessageList(Static):
     def compose(self):
-        yield Messages("List of messages")
+        yield Messages(id="messages")
         yield ButtonBar()
 
 
-class MessagePane(Static):
+class MessagePane(ScrollableContainer):
     pass
 
 
 class MainPanel(Static):
     def compose(self):
         yield MessageList()
-        yield MessagePane("Message text is here.")
+        yield MessagePane()
 
 
 class GMailApp(App):
@@ -41,6 +68,33 @@ class GMailApp(App):
         yield Header()
         yield MainPanel()
         yield Footer()
+
+    def on_messages_mounted(self, message):
+        self.update_messages()
+
+    async def on_mount(self):
+        # self.update_messages()
+
+        with open(pathlib.Path("~/.gmail_tui/conf.toml").expanduser(), "rb") as f:
+            self.config = tomllib.load(f)
+
+    @work(exclusive=True, thread=True)
+    def update_messages(self):
+        print("Updating messages ...")
+        messages_widget = self.query_one("#messages")
+        credentials = get_gmail_credentials(self.config)
+        print("Got credentials.")
+        messages, next_page_token = list_messages(credentials)
+        print("Got messages.")
+        messages_widget.remove_children()
+        for item in messages:
+            print(item)
+            dt = parse_date(item["Date"])
+            date_str = dt.isoformat()
+            sender = item["From"]
+            subject = item["Subject"]
+            widget = MessageItem(date_str, sender, subject)
+            self.call_from_thread(messages_widget.mount, widget)
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
